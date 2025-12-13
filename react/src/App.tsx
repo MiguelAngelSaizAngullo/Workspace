657c620d73fde979e0d8a435ef4885750875f919
Miguel Angel Saiz <miguelsaizangull@gmail.com>
2025-12-13 20:38:13 +0100

    Create the first app with react for studie

---
+++ react/src/App.tsx
@@ -0,0 +1,8 @@
+import "./App.css";
+import Titulo from "./Titulo";
+
+function App() {
+  return <Titulo></Titulo>;
+}
+
+export default App;
---
+++ react/src/Titulo.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function Titulo() {
+  const nombre = "Chanchito Feliz";
+
+  if (nombre) {
+    return <h1>Hola {nombre}</h1>;
+  }
+  return <h1>Hola mundo!</h1>;
+}
+
+export default Titulo;
