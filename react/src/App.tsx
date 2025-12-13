89ca595f9c632263a4396d46dcc1c11f206ff186
Miguel Angel Saiz <miguelsaizangull@gmail.com>
2025-12-14 00:57:46 +0100

    create the first component Card.tsx

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,8 +1,8 @@
 import "./App.css";
-import Titulo from "./Titulo";
+import Card from "./components/Card";
 
 function App() {
-  return <Titulo></Titulo>;
+  return <Card></Card>;
 }
 
 export default App;
